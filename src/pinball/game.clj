fb774ba0c884d912a59853df7594f052cf610bf3
joshuacalloway <joshua.calloway@gmail.com>
2013-10-17 13:13:24 -0500

    changed to seesaw

--- src/pinball/game.clj
+++ src/pinball/game.clj
@@ -1,56 +1,30 @@
 (ns pinball.game
-  (:import (java.awt Color Dimension)
-           (javax.swing JPanel JFrame JOptionPane)
-           (java.awt.event ActionListener KeyListener))
-  )
+    (:use seesaw.core seesaw.border)
+    (:import (java.awt Color))
+    )
 
 (require ['pinball.ball :as 'ball])
 (require ['pinball.position :as 'position])
 
-(defn- quit-key? [c]
-  (= \q c)
-  )
-(defstruct controls :magnification :center :trails :clear)
-(def center (position/make 500 500))
-
-(defn handle-key [c game controls]
-  (cond
-   (quit-key? c) (System/exit 1)
-   ))
-
-(defn game-panel [frame game controls]
-  (proxy [JPanel ActionListener KeyListener] []
-    (keyPressed [e]
-      (handle-key (.getKeyChar e) game controls)
-      (.repaint this)
-      )
-    (keyReleased [e])
-    (keyTyped [e])
-    (getPreferredSize []
-      (Dimension. 500 500)
-      )
-    
-    )
+(defn game-gui []
+  (invoke-later
+  (-> (frame :title "Pinball Demo"
+             :content (vertical-panel
+                       :background Color/GRAY
+                       :border (line-border :color Color/BLACK :thickness 5)
+                       :items [(label :border (line-border)
+                                      :text "this label"
+                                      :id
+                                      :link
+                                      :cursor
+                                      :hand)
+                       (button :id :button :text "This")
+                       ])
+             :on-close :exit)
+    pack!
+    show!))
   )
 
-  
-(defn game-frame []
-  (let [
-        controls (ref (struct-map controls
-                        :center center
-                        :clear false))
-        game nil
-        frame (JFrame. "Pinball")
-        panel (game-panel frame game controls)
-        ]
-    (doto panel
-      (.setFocusable true)
-      (.addKeyListener panel)
-      )
-    (doto frame
-      (.add panel)
-      (.pack)
-      (.setVisible true)
-      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))))
 
-(defn run-game [] (game-frame))
+      
+(defn run-game [] (game-gui))
